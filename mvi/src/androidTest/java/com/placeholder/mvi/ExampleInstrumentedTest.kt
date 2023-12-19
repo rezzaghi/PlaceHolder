35a1ad91aed2270d2c8e27e1506c637b6ea5a935
rezzaghi <lbrezzaghi@gmail.com>
2023-12-19 19:20:14 -0300

    tests

--- mvi/src/androidTest/java/com/placeholder/mvi/ExampleInstrumentedTest.kt
+++ mvi/src/androidTest/java/com/placeholder/mvi/ExampleInstrumentedTest.kt
@@ -2,11 +2,21 @@ package com.placeholder.mvi
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.placeholder.mvi.data.model.Todos
+import com.placeholder.mvi.data.repository.PlaceHolderRepository
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import javax.inject.Inject
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +24,28 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+@HiltAndroidTest
+class InstrumentedTests {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var repo: PlaceHolderRepository
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.placeholder.mvi", appContext.packageName)
+    fun testPlaceHolderCall() = runBlocking {
+        val todoResult = repo.getTodos().first()
+        val todoMock = Todos(
+            userId = 1,
+            id = 1,
+            title = "delectus aut autem",
+            completed = false
+        )
+        assertEquals(todoMock, todoResult)
     }
 }
--- mvi/src/main/java/com/placeholder/mvi/di/modules.kt
+++ mvi/src/main/java/com/placeholder/mvi/di/modules.kt
@@ -5,16 +5,18 @@ import com.placeholder.mvi.data.repository.PlaceHolderRepository
 import com.placeholder.mvi.data.repository.PlaceHolderRepositoryImpl
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 
 @HiltAndroidApp
 class MviApplication : Application() {
 }
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class PlaceHolderModule {
     @Binds
     abstract fun bindPlaceHolderRepository(impl: PlaceHolderRepositoryImpl): PlaceHolderRepository
